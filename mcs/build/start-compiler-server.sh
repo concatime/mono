fc09711d9a08973a2c2fbfe6cf84f90ad9a9ff7f
Issam Maghni <concatime@users.noreply.github.com>
2022-02-15 15:32:39 -0500

    Use POSIX shell instead of bash

--- mcs/build/start-compiler-server.sh
+++ mcs/build/start-compiler-server.sh
@@ -1,19 +1,18 @@
-#!/usr/bin/env bash
+#!/bin/sh
 # usage: start-compiler-server.sh <working directory> <log path> <pipename>
 # ensure that VBCS_RUNTIME and VBCS_LOCATION environment variables are set.
 
-set -u
-set -e
+set -eu
 
-if [ -s "$VBCS_LOCATION" ]; then
+if test -s "$VBCS_LOCATION"; then
     CMD="RoslynCommandLineLogFile=$2 $VBCS_RUNTIME --gc-params=nursery-size=64m \"$VBCS_LOCATION\" -pipename:$3 &"
+    command -v sh >/dev/null 2>&1 && SH=sh || SH=/bin/sh
     echo "Log location set to $2"
     touch "$2"
-    echo "cd $1; bash -c \"$CMD\""
+    echo "cd $1; $SH -c \"$CMD\""
     cd "$1"
-    bash -c "$CMD"
-    RESULT=$?
-    if [ $RESULT -eq 0 ]; then
+    "$SH" -c "$CMD"
+    if test "$?" -eq 0; then
         echo Compiler server started.
     else
         echo Failed to start compiler server.
